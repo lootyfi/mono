3c9b631ae782dd00f5e7525f756f4946849c8306
marsic3 <arsic.milos30@gmail.com>
2023-12-18 19:47:50 +0100

    cursor-not-allowed

--- apps/looty/app/components/home/banner.tsx
+++ apps/looty/app/components/home/banner.tsx
@@ -15,7 +15,7 @@ export const Banner = () => {
             {isSmallDevice ?
                 (<div className='relative flex flex-col w-full mx-auto max-w-7xl items-center justify-center my-12 lg:my-32'>
                     <Image src="/images/deal-banner-mobile.svg" height={594} width={350} alt="Key treasury" />
-                    <button className="text-[#B73FFF] text-[18px] font-bold bg-white rounded-2xl px-10 py-5 absolute bottom-72 lg:bottom-10 left-[52px] lg:left-[70px]"
+                    <button className="cursor-not-allowed text-[#B73FFF] text-[18px] font-bold bg-white rounded-2xl px-10 py-5 absolute bottom-72 lg:bottom-10 left-[52px] lg:left-[70px]"
                     // onClick={() => { }}
                     >
                         Mint now
@@ -25,7 +25,7 @@ export const Banner = () => {
                 <div className='relative flex flex-col w-full mx-auto max-w-7xl items-center justify-center my-12 lg:my-32'>
                     <Image src="/images/deal-banner.svg" height={399} width={1400} alt="Key treasury" />
 
-                    <button className="text-[#B73FFF] text-[18px] font-bold bg-white rounded-2xl px-10 py-5 absolute bottom-72 lg:bottom-10 left-[52px] lg:left-[70px]"
+                    <button className="cursor-not-allowed text-[#B73FFF] text-[18px] font-bold bg-white rounded-2xl px-10 py-5 absolute bottom-72 lg:bottom-10 left-[52px] lg:left-[70px]"
                     // onClick={() => { }}
                     >
                         Mint now
